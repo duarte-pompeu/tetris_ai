6acdeb6c3adc82b9d0dcffb02209d804d09a3927
duarte-pompeu <ist173008@lab14p8.rnl.ist.utl.pt>
2015-10-21 23:04:41 +0100

    implement all functions todo: test more throughly

--- tetris.lisp
+++ tetris.lisp
@@ -59,12 +59,12 @@
 
 (defun cria-tabuleiro ()
 	(let ((tabuleiro (make-tabuleiro)))
-		
+
 		(setf (tabuleiro-campo-jogo tabuleiro)
 			(make-array '(18 10)))
 		(setf (tabuleiro-altura-colunas tabuleiro)
 			(make-array `(10) :initial-element 0))
-		
+
 		tabuleiro)
 )
 
@@ -74,32 +74,42 @@
 		(campo-velho (tabuleiro-altura-colunas tabuleiro-velho))
 		(campo-novo (tabuleiro-altura-colunas tabuleiro-novo))
 		(altura-colunas-velho (tabuleiro-altura-colunas tabuleiro-velho))
-		(altura-colunas-novo (tabuleiro-altura-colunas tabuleiro-novo)))
-		
+		(altura-colunas-novo (tabuleiro-altura-colunas tabuleiro-novo))
+		(max-altura (reduce #'max altura-colunas-velho)))
+
 		(loop for i from 0 to (- (array-dimension altura-colunas-novo 0) 1)
 			do (setf (aref altura-colunas-novo i) (aref altura-colunas-velho i)))
-		
+
 		;TODO: copiar campo de jogo
 		; usar tabuleiro-altura-coluna para reduzir nº de ciclos
+		(loop for coluna upto  (- (array-dimension altura-colunas-velho 0) 1)
+			for linha upto (- (tabuleiro-altura-coluna tabuleiro coluna) 1)
+		do (setf campo-novo linha coluna
+			(aref campo-velho linha coluna)))
+
 		tabuleiro-novo)
 )
 
 (defun tabuleiro-preenchido-p (tabuleiro n-linha n-coluna)
-	(cond ((< (tabuleiro-altura-coluna n-coluna) n-linha)
-			nil)
-		(t (progn
-			
-		)))
+	(aref (tabuleiro-campo-jogo tabuleiro) n-linha n-coluna)
 )
 
 (defun tabuleiro-altura-coluna (tabuleiro n-coluna)
 	(aref (tabuleiro-altura-colunas tabuleiro) n-coluna)
 )
 
-; testes funcoes tabuleiro 1-4
-
+; teste funcoes tabuleiro 1-4
 (defun teste-tabuleiro1-4 ()
 	(let* ((tab1 (cria-tabuleiro))
-		((tab2 (copia-tabuleiro tab1))))
+		(tab2 (copia-tabuleiro tab1)))
+		
+		(setf (tabuleiro-campo-jogo tab1)
+			(make-array '(18 10) :initial-element 5))
+		(setf (tabuleiro-altura-colunas tab1)
+			(make-array `(10) :initial-element 3))
+			
+		(mylog tab1)
+		; these should differ: create table 1, copy to table2, modify table 2
+		(mylog tab2)
 	)
 )
