fd31695ff89482d1663b16f139df4065f0bd60e0
duarte-pompeu <pnt12@live.com.pt>
2015-11-22 16:57:35 +0000

    add way to chose between print modes - described in testa-pp.lisp

--- testa-pp.lisp
+++ testa-pp.lisp
@@ -1,24 +1,40 @@
-; dica: isto faz load a todos os ficheiros (devido ao (rl))
-; e interpreta uma ou varias funcoes e envia para o less
-; exemplo para correr o teste pp2
 ; clisp -i "tetris.lisp" -x "(rl) (pp2)" | less
+; faz load a todos os comandos (rl) e executa teste (pp2 neste exemplo)
+
+
+; grupo-executa-jogada - imprime tudo de uma vez
+; executa-jogadas (utils.lisp) - interactivo
+; para escolher: comentar a funcao a nao usar
+(defun desenha-jogada ()
+	(function grupo-executa-jogadas)
+	;(function executa-jogadas)
+)
+
 
 (defun testa-pp (lista-pecas)
 	(let* ((estado (cria-estado lista-pecas))
 		(sequencia-solucao
 			(procura-pp (cria-problema estado nil))))
 
-	(loop for peca in sequencia-solucao
-	do (progn
-		(setf estado (resultado estado peca))
-		(desenha-estado estado)))
+		(funcall (desenha-jogada) estado sequencia-solucao)
 ))
+
 
 (defun pp1 ()
 	(testa-pp '(o o))
 )
 
+
 (defun pp2 ()
 	(testa-pp '(o o o o o o o o o o o o o o o o o o o o o o o o o o o
 		o o o o o o o o o o o o o o o o o o o o o ))
 )
+
+
+(defun grupo-executa-jogadas (estado-inicial sequencia-jogadas)
+	(let ((estado estado-inicial))
+		(loop for peca in sequencia-jogadas
+		do (progn
+			(setf estado (resultado estado peca))
+			(desenha-estado estado)))
+))
