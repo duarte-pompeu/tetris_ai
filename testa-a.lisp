2ff362d8164fc2801358a6ee2f5e0c2732296e07
duarte-pompeu <pnt12@live.com.pt>
2015-11-30 18:36:44 +0000

    2 testes para procura best

--- testa-a.lisp
+++ testa-a.lisp
@@ -16,7 +16,30 @@
 	 sequencia-solucao
 ))
 
+
+(defun testa-best (tabuleiro lista-pecas )
+	(let* ((estado (cria-estado lista-pecas))
+		(sequencia-solucao nil))
+		
+		(if tabuleiro
+			(setf (estado-tabuleiro estado) tabuleiro))
+	
+	(setf sequencia-solucao (procura-best (tabuleiro->array tabuleiro) lista-pecas))
+	(desenha-estado estado)
+	(funcall (desenha-jogada) estado sequencia-solucao)
+	 
+	 sequencia-solucao
+))
+
 ; com 8 o's e 2 i's num tabuleiro vazio, resultado esperado sao 800 pontos
 (defun pa*1 ()
 	(testa-a* '(o o o o o o o o i i))
+)
+
+(defun pb1 ()
+	(testa-best (cria-tabuleiro) '(o i i))
+)
+
+(defun pb2 ()
+	(testa-best (cria-tabuleiro) '(o o o o o o o o i i))
 )
